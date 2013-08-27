3f715bf1ef9d1164d4202a7b8e18113ff51bc81a
Peter Mears <petermears@PmMacBook-Pro.local>
2013-08-27 23:42:47 +0900

    new variation

---
+++ vnc-server.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+### BEGIN INIT INFO
+# Provides: vncserver
+# Required-Start:
+# Required-Stop:
+# Default-Start: 2 3 4 5
+# Default-Stop: 0 1 6
+# Short-Description: start vnc server
+# Description:
+### END INIT INFO
+
+. /lib/lsb/init-functions
+
+# Carry out specific functions when asked to by the system
+case "$1" in
+  start)
+    su root -c 'vncserver :1 -geometry 1280x800 -depth 24'
+    echo "Starting VNC server "
+    ;;
+  stop)
+    pkill Xtightvnc
+    echo "VNC Server has been stopped (didn't double check though)"
+    ;;
+  *)
+    echo "Usage: /etc/init.d/vncserver {start|stop}"
+    exit 1
+    ;;
+esac
